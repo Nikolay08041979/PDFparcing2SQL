0ae4260b0402617d0bceb56ba0c2773fa1130360
Nikolay Mazur <nmazur79@gmail.com>
2024-02-23 14:42:53 +0300

    Files updated 23Feb2024

--- code_script/main.py
+++ code_script/main.py
@@ -1,9 +1,13 @@
+from code_script.dataframe_create import get_json_print_none
 from dataframe_export import export_dataframe_to_sql, get_count_sql
+#from code_script.dataframe_create import get_data_frame_from_json
 
 def samarry():
+    get_json_print_none()
     export_dataframe_to_sql()
     get_count_sql()
     return
 
 if __name__ == '__main__':
-    samarry()
+    print(samarry())
+
